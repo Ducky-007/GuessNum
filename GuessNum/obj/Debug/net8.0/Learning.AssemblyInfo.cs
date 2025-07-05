5122a1a5c4ffac49f6427f6c488b807c4360b82a
Ducky-007 <c.duckett93@gmail.com>
2025-07-05 08:51:49 -0700

    changes file names

--- GuessNum/obj/Debug/net8.0/Learning.AssemblyInfo.cs
+++ GuessNum/obj/Debug/net8.0/Learning.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learning")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f2673c58d921ac21821c2ec3e57b417b0297b3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Learning")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
